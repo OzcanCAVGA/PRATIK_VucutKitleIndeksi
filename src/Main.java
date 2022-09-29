7287d8b1f0b4c259c3a86a5f0810705352751054
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:14:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+/*
+Vücut Kitle İndeksi Hesaplama
+Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın. Aşağıdaki formüle göre kullanıcının
+"Vücut Kitle İndeks" değerini hesaplayıp ekrana yazdırın.
+
+Formül
+Kilo (kg) / Boy(m) * Boy(m)
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        float kilo, boy, vki;
+        Scanner yazici = new Scanner(System.in);
+
+        System.out.println("Lutfen boyunuzu girin(metre cinsinden): ");
+        boy = yazici.nextFloat();
+        System.out.println("Lutfen kilonuzu girin(KG cinsinden): ");
+        kilo=yazici.nextFloat();
+        vki = (float) (kilo / (boy * boy));
+
+        System.out.println("Vucut kitle indeksiniz: "+vki);
+
     }
 }
